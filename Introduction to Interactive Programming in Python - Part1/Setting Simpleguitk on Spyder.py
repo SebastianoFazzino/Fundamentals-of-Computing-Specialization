a4009acae4e8046a7b93ce202eb8a31d070fef76
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-10-20 11:47:54 +0300

    Add files via upload

---
+++ Introduction to Interactive Programming in Python - Part1/Setting Simpleguitk on Spyder.py
@@ -0,0 +1,25 @@
+#in order to use simplegui on Spyder IDE, I had to first pip install it on anaconda prompt:
+# pip install SimpleGUITk.
+#Find more information about SimpleGUITk on this website: https://pypi.org/project/SimpleGUITk/
+
+
+import simpleguitk as simplegui
+
+message = "Welcome!"
+
+# Handler for mouse click
+def click():
+    global message
+    message = "Good job!"
+
+# Handler to draw on canvas
+def draw(canvas):
+    canvas.draw_text(message, [50,112], 36, "Red")
+
+# Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("Home", 300, 200)
+frame.add_button("Click me", click)
+frame.set_draw_handler(draw)
+
+# Start the frame animation
+frame.start()
