9c69a1b779fa6ee8791c7a9c75f041ac28cbb98e
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-10-22 12:05:36 +0300

    Add files via upload

---
+++ Introduction to Interactive Programming in Python - Part1/Projects/Guess the Number Project.py
@@ -0,0 +1,108 @@
+#we start importing the modules needed
+import math
+import random
+import simpleguitk as simplegui
+
+#we define the global variables
+secret_number = int
+number_of_guesses = 7
+Range = 100
+width = 150
+
+def new_game():
+    global number_of_guesses
+    '''this helper function gets invoked every time a new game starts, when the range changes from 100 to 1000 and viceversa,
+    if the user input is not an int or if is a number out of range, if the user guesses the number or if he runs out of attempts'''
+    print("\nNew game, range is from 0 to", Range)
+    print("Number of remaining guesses is", number_of_guesses)
+    
+
+
+def range100():
+    '''this function sets the secret_number range to 100, compute the number of attempts and invokes new_game function'''
+    global secret_number, number_of_guesses, Range
+    Range = 100
+    secret_number = random.randrange(0, Range)
+    #using the math module, we compute n guesses where n is the smallest integer such that 2 ** n >= high - low + 1
+    #we use ceil() function to round up the number obtained and we calculate the logarithm of given range, base 2
+    number_of_guesses = math.ceil(math.log(Range, 2))
+    new_game()
+    
+    
+
+
+def range1000():
+    '''this function sets the secret_number range to 1000, compute the number of attempts and invokes new_game function'''
+    global secret_number, number_of_guesses, Range
+    Range = 1000
+    secret_number = random.randrange(0, Range)
+    number_of_guesses = math.ceil(math.log(Range, 2))
+    new_game()
+    
+    
+
+def choose_range(num):
+    '''This function allows the user to chose a desired range'''
+    global secret_number, number_of_guesses, Range
+    Range = int(num)
+    secret_number = random.randrange(0, Range)
+    number_of_guesses = math.ceil(math.log(Range, 2))
+    new_game()
+
+    
+
+def input_guess(guess):
+    '''this function determines if the user input is a valid number and if not it restarts the game
+    if the input is a valid number, it checks if it's the equal to the secret number, if not, it decrease the user 
+    number of attempts left by 1. Once the user guesses the secret number or the number of attempts reaches 0, the game restarts'''
+    global secret_number, number_of_guesses, Range
+    
+    try:
+       your_guess = int(guess)
+    
+    
+       if your_guess < 0 or your_guess > Range:
+          print("\n*** Please enter a number between 0 and", Range, "***")
+          range100()
+       
+       else:
+        
+           print("\nYour guess was:", your_guess)
+    
+           if your_guess > secret_number and number_of_guesses > 0:
+              print("Lower!")
+              number_of_guesses -= 1
+              print("Number of remaining guesses is", number_of_guesses)
+            
+           if your_guess < secret_number and number_of_guesses > 0:
+              print("Higher!")
+              number_of_guesses -= 1
+              print("Number of remaining guesses is", number_of_guesses)
+            
+           if your_guess == secret_number and number_of_guesses > 0:
+              print("\n*** Correct! ***")
+              range100()
+            
+           if number_of_guesses == 0:
+              print("You ran out of guesses attempts!")
+              print("*** Secret Number was:", secret_number, "***")
+              range100()
+    
+    except:
+        print("\n*** Incorrect Input! ***")
+        range100()
+      
+    
+    
+#we create the frame, buttons and input 
+frame = simplegui.create_frame("Guess the Number!", 200, 200)
+frame.add_button("Range 0-100", range100, width)
+frame.add_button("Range 0-1000", range1000, width)
+frame.add_input("Choose your Range!", choose_range, 176)
+frame.add_button("Start New Game", range100, width)
+frame.add_input("Enter your number here!", input_guess, 176)
+    
+#we invoke range100() and we start the frame
+range100()
+frame.start()
+
