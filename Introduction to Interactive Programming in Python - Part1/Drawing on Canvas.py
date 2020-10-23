5624f626d90d359928f69b747acf9ed69c6aabf2
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-10-23 10:00:44 +0300

    Update Drawing on Canvas.py

--- Introduction to Interactive Programming in Python - Part1/Drawing on Canvas.py
+++ Introduction to Interactive Programming in Python - Part1/Drawing on Canvas.py
@@ -51,3 +51,24 @@ frame2.set_canvas_background("Yellow")
 
 # Start the frame animation
 frame2.start()
+
+
+
+
+
+
+#let's drive a truck on the canvas using circles and lines
+
+def draw3(canvas):
+    canvas.draw_circle((90,200), 20, 10, "white")
+    canvas.draw_circle((210,200), 20, 10, "white")
+    canvas.draw_line((50, 180), (250, 180), 40, "red")
+    canvas.draw_line((55, 170), (90, 120), 5, "red")
+    canvas.draw_line((90, 120), (130, 120), 5, "red")
+    canvas.draw_line((180, 108), (180, 160), 140, "red")
+    
+    
+frame3 = simplegui.create_frame("sample", 300, 300)
+frame3.set_draw_handler(draw)
+
+frame3.start()
