a7f59ce7a85daebf970d9d7142f4adedd2c28885
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-10-23 08:47:50 +0300

    Add files via upload

---
+++ Introduction to Interactive Programming in Python - Part1/Interactive Drawing.py
@@ -0,0 +1,67 @@
+#interactive application to convert a float into dollars and cents
+
+#import simpleguitk
+import simpleguitk as simplegui
+
+#define global variables
+value = 1.0
+
+def convert_units(val, name):
+    result = str(val) + " " + name
+    if val > 1:
+        result = result + "s"
+    return result
+        
+# convert xx.yy to xx dollars and yy cents
+def convert(val):
+    # Split into dollars and cents
+    dollars = int(val)
+    cents = int(round(100 * (val - dollars)))
+
+    # Convert to strings
+    dollars_string = convert_units(dollars, "dollar")
+    cents_string = convert_units(cents, "cent")
+
+    # return composite string
+    if dollars == 0 and cents == 0:
+        return "Broke!"
+    elif dollars == 0:
+        return cents_string
+    elif cents == 0:
+        return dollars_string
+    else:
+        return dollars_string + " and " + cents_string
+
+
+    
+    
+#define draw handler
+def draw(canvas):
+    canvas.draw_text(convert(value), [60, 110], 18, "white")
+
+
+#define an input field handler
+def input_handler(text):
+    global value
+    value = float(text)
+    
+    
+
+#create a frame
+frame = simplegui.create_frame("converter", 370, 200)
+
+
+#register event handlers
+frame.set_draw_handler(draw)
+frame.add_input("Enter value", input_handler, 150)
+
+#start frame
+frame.start()
+
+
+
+
+
+
+
+
